185a416cb4646b585609856333a385cf13b95bac
Rushikesh Darge <39642887+rushidarge@users.noreply.github.com>
2022-06-21 15:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import tensorflow
-from PIL import Image
 from tensorflow.keras.models import load_model
 import numpy as np
 import time
 import cv2
+from PIL import Image
 
 
 @st.cache(allow_output_mutation=True)
@@ -14,14 +14,17 @@ def get_model():
     
 def predict(image):
         loaded_model = get_model()
-        image = cv2.imread(image)
-        image = cv2.resize(image/255, (224, 224))
+        size = (224,224)    
+        image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
+        image = np.asarray(image)/255
+        # image = cv2.imread(image)
+        # image = cv2.resize(image/255, (224, 224))
         image = np.reshape(image,[1,224,224,3])
 
         classes = loaded_model.predict_classes(image)
         return classes 
 
-st.title("NSFW Classifier")
+st.title("SaferNet with AI")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
